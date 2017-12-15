755df7588b849a1cf898124bda5e56c66aa1a405
Nikos Linakis <nlinakis@gmail.com>
2017-12-15 21:23:50 +0200

    Added option to limit logging on wifi only

--- timber-loggly/src/main/java/com/github/tony19/timber/loggly/LogglyTree.java
+++ timber-loggly/src/main/java/com/github/tony19/timber/loggly/LogglyTree.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Anthony K. Trinh
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,8 +15,18 @@
  */
 package com.github.tony19.timber.loggly;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
+
 import com.github.tony19.loggly.ILogglyClient;
 import com.github.tony19.loggly.LogglyClient;
+
 import timber.log.Timber;
 
 /**
@@ -30,6 +40,11 @@ public class LogglyTree extends Timber.Tree {
     private ILogglyClient loggly;
     private ILogglyClient.Callback handler;
     private IFormatter formatter;
+
+    private boolean isLimitWifi = false;
+
+    private Context context;
+    private ConnectivityManager connectivityManager;
 
     /**
      * Creates a <a href="https://github.com/JakeWharton/timber">Timber</a>
@@ -56,6 +71,37 @@ public class LogglyTree extends Timber.Tree {
     }
 
     /**
+     * Limits the {@link LogglyTree} to only send logs while connected to WiFi or suppress them otherwise.
+     * @param context Context
+     * @return a {@link LogglyTree} that only logs over WiFi
+     */
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+    public LogglyTree limitWifi(@NonNull Context context) {
+        isLimitWifi = true;
+
+        this.context = context;
+        this.connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return this;
+    }
+
+    /**
+     * Determines if the device is connected to a WiFi network.
+     * @return true if device is connected to WiFi or false otherwise
+     */
+    @SuppressLint("MissingPermission")
+    private boolean isOnWifi() {
+        if(context.checkCallingOrSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+                == PackageManager.PERMISSION_GRANTED) {
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+                return activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+            }
+        }
+        return false;
+    }
+
+    /**
      * Writes a log message to its destination. Called for all level-specific methods by default.
      *
      * @param priority Log level. See {@link android.util.Log} for constants.
@@ -65,7 +111,9 @@ public class LogglyTree extends Timber.Tree {
      */
     @Override
     protected void log(int priority, String tag, String message, Throwable t) {
-        loggly.log(formatter.format(priority, tag, message, t), handler);
+        if (!isLimitWifi || isOnWifi()) {
+            loggly.log(formatter.format(priority, tag, message, t), handler);
+        }
     }
 
     /**
